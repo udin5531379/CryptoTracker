cd9a635615b426e0119bdc590a9aeaafbc211143
Udin Rajkarnikar <udin_rajkarnikar2001@yahoo.com>
2022-10-13 11:49:29 -0500

    Project Setup

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Udin Rajkarnikar on 10/13/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
